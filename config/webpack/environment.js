1f9f7e2d5da50f497fcf1e84898d3bf8dc92cd53
Robert253 <Robert253web@gmail.com>
2021-03-09 16:29:05 +0000

    Basic Devise

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.append('provide',
+new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['Popper.js', 'default']
+  })
+)
+
 module.exports = environment
