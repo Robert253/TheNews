0cbac47e0c931fb86a9a2e360aa540c4648b0445
Robert253 <Robert253web@gmail.com>
2021-04-09 12:39:35 +0100

    Publisher dashboard/Posts SCSS

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  # Db query making posts in order
+  scope :most_recent, -> { order(id: :desc)}
+
+  # Method for pushing date ('%-b %-d, %y') formatting date
+  # putting variable inside string
+  def display_day_published
+    "Published : #{created_at.strftime('%-b %-d, %Y')}"
+  end
 end
