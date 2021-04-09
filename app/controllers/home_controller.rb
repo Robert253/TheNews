b61737ad4f49155b483492b7bfd7179356fab15c
Robert253 <Robert253web@gmail.com>
2021-04-09 17:58:05 +0100

    User Profile/changes to html

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+  before_action :load_posts, only: :index
+
   def index
+    #logic for your index
+  end
+
+  private
+
+  def load_posts
+    @posts = Post.order("created_at desc").limit(6)
   end
 end
